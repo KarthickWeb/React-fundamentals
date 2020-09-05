56258097ecaa124587bfb9e8a47750f8d99f340d
Karthickweb <karthiashok5604@gmail.com>
2020-09-05 09:09:33 -0700

    header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default class Head extends Component {
                     <div>
                         <h1><span>Karthick</span><br />Web Developer</h1>
                         <p> I am fresher in the Corporate culture, but ready to accept the challenges, utlizing my skills, would like to work with a highly esteemed company which gives me a platfrom to use my expertise and skills for mutual growth of company and myself.  </p>
-                        <button><a href="#" className="text-uppercase">contact me</a></button>
+                        <button><a href=" " className="text-uppercase">contact me</a></button>
                     </div>
                 </div>
             </div>
